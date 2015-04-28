7ab8a2e4fc0c97ac6b737c2b73d95d23cb7e40cf
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-28 20:40:29 +0100

    InCallUI: Add a controller for in-call proximity sensor (2/3)
    follow up to this patch
    https://github.com/Euphoria-OS/android_packages_apps_InCallUI/commit/9cc23a9201b7580b0cbb6018ba62b0b06b8f1651
    
    Change-Id: If3d581486c5776b1d030e1087bdf664be468115a

--- src/com/android/incallui/ProximitySensor.java
+++ src/com/android/incallui/ProximitySensor.java
@@ -44,8 +44,6 @@ public class ProximitySensor implements AccelerometerListener.OrientationListene
         InCallStateListener, AudioModeListener {
     private static final String TAG = ProximitySensor.class.getSimpleName();
 
-    private static final String PROXIMITY_SENSOR = "proximity_sensor";
-
     private final PowerManager mPowerManager;
     private final PowerManager.WakeLock mProximityWakeLock;
     private final AudioModeProvider mAudioModeProvider;
@@ -238,7 +236,7 @@ public class ProximitySensor implements AccelerometerListener.OrientationListene
                     || CallAudioState.ROUTE_BLUETOOTH == audioMode
                     || mIsHardKeyboardOpen);
             screenOnImmediately |= Settings.System.getInt(mContext.getContentResolver(),
-                    PROXIMITY_SENSOR, 1) == 0;
+                    Settings.System.IN_CALL_PROXIMITY_SENSOR, 1) == 0;
 
             // We do not keep the screen off when the user is outside in-call screen and we are
             // horizontal, but we do not force it on when we become horizontal until the
